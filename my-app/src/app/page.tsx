021f36a8be7692eaf99836576bfcdac65256094a
RubiAbid <rubiridz@gmail.com>
2025-02-08 00:03:55 +0500

    Added hero section

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,16 +1,15 @@
 import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
 import Featured from "@/components/Featured"
-import Image from "next/image";
 import Featured2 from "@/components/Featured2";
+import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
     <div>
       <Navbar/>
       <Header/>
-      <Featured/>
-      <Featured2/>
+      <HeroSection/>
     </div>
   );
 }
